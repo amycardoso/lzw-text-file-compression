ad923c72d49b93332b3c76a6e4c1b23ebb2e409c
amycardoso <alice.cardoso10@gmail.com>
2018-10-26 17:24:24 -0500

    Update

--- lzw.py
+++ lzw.py
@@ -7,11 +7,12 @@ def compressao(entrada):
     tamanhoDicionario = 256
     dicionario = {} #armazena o dicionário
     resultado = [] #armazena o resultado comprimido
-    temp = "" #variável que irá armazenar as strings
+    temp = "" #variável que irá armazenar as substrings
 
     #Adicionando tabela ASCII ao dicionário
     for i in range(0, tamanhoDicionario):
         dicionario[str(chr(i))] = i #Como fica o dicionário {'a': 97, 'b': 98}
+    #assim podemos percorrer o arquivo, e substituir as substrings por seus respectivos inteiros
 
     for c in entrada: #Percorre o arquivo
         temp2 = temp+str(chr(c)) #temp2 recebe o caractere atual mais o anterior para verificar se existe no dicionário
@@ -21,7 +22,7 @@ def compressao(entrada):
             resultado.append(dicionario[temp]) #pegamos o inteiro que representa a string anterior no dicionário e adicionamos ao resultado
             dicionario[temp2] = tamanhoDicionario #em seguida adicionamos temp2 ao dicionário
             tamanhoDicionario+=1 #incrementa tamanho do dicionário
-            temp = ""+str(chr(c)) #reseta a string temporária com o caractere atual
+            temp = ""+str(chr(c)) #reseta a variável com a substring atual
 
     if temp != "": #caso a string temporária não esteja vazia, deve-se adicionar ao resultado
         resultado.append(dicionario[temp]) #pegando o inteiro que a representa no dicionário    
@@ -36,7 +37,7 @@ def descompressao(entrada):
 
     #inicializando dicionário com tabela ASCII
     for i in range(0, tamanhoDicionario):
-        dicionario[i] = str(chr(i)) #Como o dicionário fica, ex: {97: 'a', 98: 'b'}
+        dicionario[i] = str(chr(i)) #Como o dicionário fica, ex: {97: 'a', 98: 'b'}, diferente do dicionário de compressão
 
     anterior = chr(entrada[0]) #pega o primeiro caractere e marca como anterior, por exemplo: chr(97) retorna a string 'a'
     entrada = entrada[1:] #remove o primeiro caractere da entrada
@@ -54,7 +55,7 @@ def descompressao(entrada):
             #o único caso em que isso pode ocorrer é se a substring começar e terminar com o mesmo caractere ("uhuhu").
                 
         resultado.append(aux) #adiciona ao resultado
-        #Resimulando como as substrings foram adicionadas durante a compactação
+        #Resimulando como as substrings foram adicionadas durante a compressão
         dicionario[tamanhoDicionario] = anterior + aux[0] #adiciona ao dicionário o caractere anterior mais a primeira posição do caractere atual
         tamanhoDicionario+= 1 #incrementa tamanho do dicionário
         anterior = aux #anterior recebe o caractere atual
